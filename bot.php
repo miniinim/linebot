d7e33480474b446984b51db01cbb86fbc60b0b1b
Mini iniM <the.miniinim@gmail.com>
2019-09-11 02:31:53 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -84,34 +84,10 @@ if ( sizeof($request_array['events']) > 0 ) {
         $send_result  = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
 
         echo "Result: ".$send_result."\r\n";
-
-        $arrayPostData['to'] = $userID;
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
-        $arrayPostData['messages'][1]['type'] = "sticker";
-        $arrayPostData['messages'][1]['packageId'] = "2";
-        $arrayPostData['messages'][1]['stickerId'] = "34";
-        pushMsg($post_header,$arrayPostData);
     }
 }
 
 echo "OK";
-
-
-function pushMsg($arrayHeader,$arrayPostData)
-{
-  $strUrl = "https://api.line.me/v2/bot/message/push";
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL,$strUrl);
-  curl_setopt($ch, CURLOPT_HEADER, false);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  $result = curl_exec($ch);
-  curl_close ($ch);
-}
 
 
 function send_reply_message($url, $post_header, $post_body)
