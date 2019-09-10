333ae250df761258b333e7b1f2285e47622d1a22
Mini iniM <the.miniinim@gmail.com>
2019-09-11 01:33:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,15 +23,12 @@ if ( sizeof($request_array['events']) > 0 ) {
         $text = $event['message']['text'];
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  //Debug Detail message
+            //'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  //Debug Detail message
             //'messages' => [['type' => 'text', 'text' => $text ]]
         ];
-        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-        //echo $send_result['events']['source']['userID']
-        echo "Result: ".$request_array['events']['source']['userID']."\r\n";
+        $post_body    = json_encode($data, JSON_UNESCAPED_UNICODE);
+        $send_result  = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+        echo "Result: ".$send_result."\r\n";
     }
 }
 
