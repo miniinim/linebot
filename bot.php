80ba56eab2a02359aad4e97510ae752eb29e6934
Mini iniM <the.miniinim@gmail.com>
2019-09-11 03:18:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -67,125 +67,8 @@ if ( sizeof($request_array['events']) > 0 ) {
             case "คะแนนธุรกิจ" :
 
             //$reply_text = "รายได้จากการพัฒนาสายงาน \nคะแนนธุรกิจ : 150,000 PV \nPersonal Bonus : 5,000 PV \nยอดขายระหว่างวันที่ 11 ส.ค. 2562 - 10 ก.ย. 2562";
+
             $jsonFlex = [
-                "type" => "flex",
-                "altText" => "Hello Flex Message",
-                "contents" => [
-                  "type" => "bubble",
-                  "direction" => "ltr",
-                  "header" => [
-                    "type" => "box",
-                    "layout" => "vertical",
-                    "contents" => [
-                      [
-                        "type" => "text",
-                        "text" => "Purchase",
-                        "size" => "lg",
-                        "align" => "start",
-                        "weight" => "bold",
-                        "color" => "#009813"
-                      ],
-                      [
-                        "type" => "text",
-                        "text" => "฿ 100.00",
-                        "size" => "3xl",
-                        "weight" => "bold",
-                        "color" => "#000000"
-                      ],
-                      [
-                        "type" => "text",
-                        "text" => "Rabbit Line Pay",
-                        "size" => "lg",
-                        "weight" => "bold",
-                        "color" => "#000000"
-                      ],
-                      [
-                        "type" => "text",
-                        "text" => "2019.02.14 21:47 (GMT+0700)",
-                        "size" => "xs",
-                        "color" => "#B2B2B2"
-                      ],
-                      [
-                        "type" => "text",
-                        "text" => "Payment complete.",
-                        "margin" => "lg",
-                        "size" => "lg",
-                        "color" => "#000000"
-                      ]
-                    ]
-                  ],
-                  "body" => [
-                    "type" => "box",
-                    "layout" => "vertical",
-                    "contents" => [
-                      [
-                        "type" => "separator",
-                        "color" => "#C3C3C3"
-                      ],
-                      [
-                        "type" => "box",
-                        "layout" => "baseline",
-                        "margin" => "lg",
-                        "contents" => [
-                          [
-                            "type" => "text",
-                            "text" => "Merchant",
-                            "align" => "start",
-                            "color" => "#C3C3C3"
-                          ],
-                          [
-                            "type" => "text",
-                            "text" => "BTS 01",
-                            "align" => "end",
-                            "color" => "#000000"
-                          ]
-                        ]
-                      ],
-                      [
-                        "type" => "box",
-                        "layout" => "baseline",
-                        "margin" => "lg",
-                        "contents" => [
-                          [
-                            "type" => "text",
-                            "text" => "New balance",
-                            "color" => "#C3C3C3"
-                          ],
-                          [
-                            "type" => "text",
-                            "text" => "฿ 45.57",
-                            "align" => "end"
-                          ]
-                        ]
-                      ],
-                      [
-                        "type" => "separator",
-                        "margin" => "lg",
-                        "color" => "#C3C3C3"
-                      ]
-                    ]
-                  ],
-                  "footer" => [
-                    "type" => "box",
-                    "layout" => "horizontal",
-                    "contents" => [
-                      [
-                        "type" => "text",
-                        "text" => "View Details",
-                        "size" => "lg",
-                        "align" => "start",
-                        "color" => "#0084B6",
-                        "action" => [
-                          "type" => "uri",
-                          "label" => "View Details",
-                          "uri" => "https://google.co.th/"
-                        ]
-                      ]
-                    ]
-                  ]
-                ]
-              ];
-            /*$jsonFlex = [
                 "type" => "flex",
                 "altText" => "รายได้จากการพัฒนาสายงาน",
                 "contents" => [
@@ -212,15 +95,8 @@ if ( sizeof($request_array['events']) > 0 ) {
                       ],
                       [
                         "type" => "text",
-                        "text" => "฿ 150,000.00",
+                        "text" => "PV 150,000.00",
                         "size" => "3xl",
-                        "weight" => "bold",
-                        "color" => "#000000"
-                      ],
-                      [
-                        "type" => "text",
-                        "text" => "Rabbit Line Pay",
-                        "size" => "lg",
                         "weight" => "bold",
                         "color" => "#000000"
                       ],
@@ -319,18 +195,19 @@ if ( sizeof($request_array['events']) > 0 ) {
                         "color" => "#0084B6",
                         "action" => [
                           "type" => "uri",
-                          "label" => "View Details",
+                          "label" => "ดูข้อมุลทั้งหมด",
                           "uri" => "https://family.confideen.com/"
                         ]
                       ]
                     ]
                   ]
                 ]
-              ];*/
+              ];
 
-              $data = [
-                  'replyToken' => $reply_token,
-                  'messages' => [$jsonFlex]
+              $data =
+              [
+                'replyToken'  => $reply_token,
+                'messages'    => [$jsonFlex]
               ];
 
             break;
@@ -362,7 +239,8 @@ if ( sizeof($request_array['events']) > 0 ) {
 
             break;
         }
-        
+
+
         $post_body    = json_encode($data, JSON_UNESCAPED_UNICODE);
         $send_result  = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
 
