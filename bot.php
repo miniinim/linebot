830739144efd93742939a2164ab68e2ed76d5c49
Mini iniM <the.miniinim@gmail.com>
2019-09-11 02:22:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,7 +29,8 @@ if ( sizeof($request_array['events']) > 0 ) {
         {
             case "Detail" :
             case "detail" :
-            case "ข้อมุล" :
+            case "ข้อมูล" :
+            case "ข้อมูลสมาชิก" :
             case "ประวัติ" :
             case "สมาชิก" :
 
@@ -82,10 +83,27 @@ if ( sizeof($request_array['events']) > 0 ) {
         $post_body    = json_encode($data, JSON_UNESCAPED_UNICODE);
         $send_result  = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
         echo "Result: ".$send_result."\r\n";
+
+        $send_push  = send_push_message($API_URL.'/push', $POST_HEADER, $post_body);
     }
 }
 
 echo "OK";
+
+
+function send_push_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
 
 
 function send_reply_message($url, $post_header, $post_body)
