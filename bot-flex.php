ed498e957d6c97359a1eacf203caecc0842b3e00
Mini iniM <the.miniinim@gmail.com>
2019-09-16 08:31:09 +0700

    Update bot-flex.php

--- bot-flex.php
+++ bot-flex.php
@@ -7,7 +7,7 @@ $POST_HEADER    = array('Content-Type: application/json', 'Authorization: Bearer
 
 $data =
 [
-  'to'        => "U512f4baa1d135d6991436b5b462826f2",
+  'to'        => "U7e31a728502ad029ba23f1b6d21a7537",
   'messages'  => [['type' => 'text', 'text' => "ระบบกำลังจัดเตรียมสินค้าที่สั่งโดยลูกค้าของนักธุรกิจ xxxx หลังจากได้รับ Tracking Number แล้ว จะมีการอัพเดตให้สมาชิกอีกครั้งค่ะ" ]]
 ];
 
